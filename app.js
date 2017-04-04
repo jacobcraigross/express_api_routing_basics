1caaf9f55295b7b675c94848111a0cb8610db3ac
Jake Ross <jakeross@Jakes-MacBook-Pro.local>
2017-04-04 10:16:24 -0500

    adding nodeRoutingParams

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+
+// "/" ---> "Hello"
+app.get("/", function(req, res){
+    res.send("hello");
+});
+
+// "/bye" ---> "Goodbye"
+app.get("/bye", function(req, res){
+   res.send("Goodbye");
+});
+
+// "/dog" ---> "Caspian"
+app.get("/dog", function(req, res){
+   res.send("Caspian");
+});
+
+// reddit-esque routing params
+app.get("/r/:subredditName", function(req, res){
+  var subreddit = req.params.subredditName;
+  res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT. . GIMME A HELL YEAH");
+});
+
+// reddit-esque routing params contd.
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+  res.send("Welcome to the comments page.");
+});
+
+app.get("*", function(req, res){
+    res.send("you are a star");
+});
+
+// starting the server from the command line
+app.listen(3000, function(){
+  console.log("the server has started.");
+});
+
+// to start the server on cloud 9
+// app.listen(process.env.PORT, process.env.IP, function(){
+    // console.log("server has started");
+// });
